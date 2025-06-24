9a8aa638d72668789f4dc60d7b476b088973f1c3
Cristina <kamuthucristina@gmail.com>
2025-06-24 13:00:11 +0300

    added the serializer rules

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import validates, relationship, Foreignkey
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 from config import db, bcrypt
@@ -6,20 +6,18 @@ from datetime import datetime
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-    serialize_rules = ('-_password_hash',)
+    serialize_rules = ('-_password_hash', '-blogs.user',)
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String, unique=True, nullable=True)
-    password_hash = db.Column(db.String, nullable=True) 
+    _password_hash = db.Column(db.String, nullable=True) 
 
-    blogs = relationship("Blog", backref="users", cascade='all, delete-orphan')  
+    blogs = relationship("Blog", backref="user", cascade='all, delete-orphan')  
 
-    
-   
     @hybrid_property
     def password_hash(self):
-        raise self._password_hash
+        raise AttributeError("Password hashes are write-only.")
 
     @password_hash.setter
     def password_hash(self, password):
@@ -29,54 +27,33 @@ class User(db.Model, SerializerMixin):
         return self._password_hash and bcrypt.check_password_hash(self._password_hash, password.encode())
 
     def __repr__(self):
-        return f"<i am user number {self.id} and my name is {self.name} >"
-    
+        return f"<i am user number {self.id} and my name is {self.username} >"
+
 
 class Blog(db.Model, SerializerMixin):
     __tablename__ = 'blogs'
+    serialize_rules = ('-user.blogs', '-tags.blogs',)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     image_url = db.Column(db.String)
     bio = db.Column(db.String, nullable=False)
-    created_at = db.column (db.String, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.Foreignkey("user.id)" ))
+    created_at = db.Column(db.String, nullable=False, default=lambda: datetime.utcnow().isoformat())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     tags = relationship("Tag", secondary="article_tags", back_populates="blogs")
 
 
 class Tag(db.Model, SerializerMixin):
     __tablename__ = 'tags'
+    serialize_rules = ('-blogs.tags',)
 
-
-    id= db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
-    articles = db.relationship("Article", secondary="article_tags", back_populates="tags")
-
-
+    blogs = relationship("Blog", secondary="article_tags", back_populates="tags")
 
 
 article_tags = db.Table("article_tags",
-                        db.Column('article_id', db.Integer, db.ForeignKey('article.id'), primary_key=True),
-                        db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True))
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  
-  
-  
-  
-  
-  
-
-
+    db.Column('blog_id', db.Integer, db.ForeignKey('blogs.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+)
